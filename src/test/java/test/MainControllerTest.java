54e406e51a1c4564ec67028308c245aed08e5a32
olzhasshaikenov <olzhas.shaikenov@gmail.com>
2016-07-16 22:37:53 -0400

    init

---
+++ src/test/java/test/MainControllerTest.java
@@ -0,0 +1,56 @@
+package test;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import restapi.Application;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class MainControllerTest {
+
+	@Autowired
+	private WebApplicationContext ctx;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void setUp() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+
+	@Test
+	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+
+		this.mockMvc.perform(get("/"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.content").value("Hello, World!"));
+	}
+
+	@Test
+	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+
+		this.mockMvc.perform(get("/").param("name", "Spring Community"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+	}
+
+}
